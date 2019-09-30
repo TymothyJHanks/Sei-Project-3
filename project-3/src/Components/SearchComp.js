8523ede963703a4d4601bf505df0d8dc2b3775ab
Tymothy J Hanks <tymjhanks@gmail.com>
2019-09-30 15:59:09 -0400

    done working on search component and need to find out how to make sure when the user searches for something that I can connnect the imput to my json data base

--- project-3/src/Components/SearchComp.js
+++ project-3/src/Components/SearchComp.js
@@ -4,18 +4,56 @@ import React, { Component } from "react";
 //Impot Home Component Here
 
 class Search extends Component {
+    constructor(props){
+        super(props)
+        //this.state object to grab the input that the user places into the search bar
+        this.state = {
+            userInput: ""
+        } 
+        //Place Binding Functions Here
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    //Place Functions Here
+    
+    //handleChange Funtion that changes the empty string into the value that the user types into the input field from the virtual dom below
+    handleChange(event) {
+        //setting the state of the key "this.state.userInput" and giving it the value of whatever the user inputs into the virtual dom
+        this.setState({
+            userInput: event.target.value
+        });
+    }
+
+    //handleSubmit function sends the user the input that they have has been submitted and what it is
+    handleSubmit(event){
+        alert(`Searching the database for ${this.state.userInput}`);
+        event.preventDefault();
+    }
+
+//Rendering the virtual dom and what displays on it
   render() {
     return (
       <div className="SearchWrapper">
-        <header>
-          <form onSubmit={}>
+          <form onSubmit={this.handleSubmit}>
           <h1>This is the Search Component</h1>
-            <input placeholder="Search Here" type="text"></input>
-            <input type="submit" value="Submit"></input>
+
+            <input 
+                placeholder="Search Here" 
+                type="text" 
+                value={this.state.userInput}
+                onChange={this.handleChange}></input>  
+
+            <input type="Submit" value="Submit"></input>
           </form>
-        </header>
+          <p></p>
       </div>
     );
   }
 }
+//exporting this file so that it can be used globally
 export default Search;
+
+//NOTES
+    //Need to figure out when I click search, I specifically search for something inside my json file
+    
