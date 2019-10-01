3ee5ac9158cd22506ed7e5500f0bb779cca12c88
Tymothy J Hanks <tymjhanks@gmail.com>
2019-10-01 16:55:02 -0400

    merge?

--- project-3/src/Components/SearchComp.js
+++ project-3/src/Components/SearchComp.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component } from "./node_modules/react";
 
 //This data should be changed with the actual json backend data to loop through it
 
