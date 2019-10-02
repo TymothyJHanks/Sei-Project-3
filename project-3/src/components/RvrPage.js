b9447e07dc0cc77a5235ac61080e3f6ba7108ae6
Zach Meadows <zmeadows1@gmail.com>
2019-10-02 15:13:39 -0400

    added like button

--- project-3/src/components/RvrPage.js
+++ project-3/src/components/RvrPage.js
@@ -1,48 +1,75 @@
 import React, { Component } from "react";
-import "../styles/RvrPage.css"
+import "../styles/RvrPage.css";
+import { ReactComponent as NoLike } from "./NoLike.svg";
+import { ReactComponent as Like } from "./Like.svg";
 
 class RvrPage extends Component {
-  constructor(props){
-    super(props)
+  constructor() {
+    super();
     this.state = {
-
-    }
+      heartButton: [<NoLike />, <Like />],
+      heartStatus: 0
+    };
+    this.likeIt = this.likeIt.bind(this);
     //bind functions here
-    this.deleteData = this.deleteData.bind(this)
+    this.deleteData = this.deleteData.bind(this);
+  }
+  likeIt(evt) {
+    evt.preventDefault();
+    this.setState({
+      heartStatus: 1
+    });
   }
   //place functions here
-  deleteData(event){
+  deleteData(event) {
     event.preventDefault();
     alert(`Deleting a Rover`);
     //Need to change fetch url when fully deployed backend is complete
-    fetch(`https://mrsrvr.herokuapp.com/delete/${this.props.roverName}`, { //check delete path controllers - `https://mrsrvr.herokuapp.com/delete/${this.}`
-        method: "DELETE",
+    fetch(`https://mrsrvr.herokuapp.com/delete/${this.props.roverName}`, {
+      //check delete path controllers - `https://mrsrvr.herokuapp.com/delete/${this.}`
+      method: "DELETE"
     })
-    .then(responce =>{
-        console.log(responce)
-    })
-    .catch(error => console.log(error));
-}
+      .then(responce => {
+        console.log(responce);
+      })
+      .catch(error => console.log(error));
+  }
   render() {
     return (
       <div className="RvrPage">
-        <img className="singlePhoto" src={this.props["img_src"]} alt={this.props.roverName} />
+        <img
+          className="singlePhoto"
+          src={this.props["img_src"]}
+          alt={this.props.roverName}
+        />
         <div className="photoData">
-          <h3>{this.props.roverName} Rover</h3>
-          <ul>
-            <li>Earth date: {this.props.earth_date}</li>
-            <li>Launch Date: {this.props.roverLaunch_date}</li>
-            <li>Landing Date: {this.props.roverLanding_date}</li>
-            <li>Status: {this.props.roverStatus}</li>
-            <li>Total Photos: {this.props.roverTotal_photos}</li>
-          </ul>
-          {/* Place a onClick  */}
-          <div className="heartButton">
-            <button>Click Here to Favorite</button>
+          <div className="nameAndLike">
+            <div className="roverName">
+              <h3>{this.props.roverName}</h3>
+            </div>
+            <div className="heartButton" onClick={this.likeIt}>
+              {this.state.heartButton[this.state.heartStatus]}
+            </div>
           </div>
-
+          <ul>
+            <li>
+              <b>Earth Date:</b> {this.props.earth_date}
+            </li>
+            <li>
+              <b>Launch Date:</b> {this.props.roverLaunch_date}
+            </li>
+            <li>
+              <b>Landing Date:</b> {this.props.roverLanding_date}
+            </li>
+            <li>
+              <b>Status:</b> {this.props.roverStatus}
+            </li>
+            <li>
+              <b>Total Photos:</b> {this.props.roverTotal_photos}
+            </li>
+          </ul>
           <div className="deleteButton">
-            <button onClick={this.deleteData} >Click Here to Delete</button>
+            <button onClick={this.deleteData}>Click Here to Delete</button>
           </div>
         </div>
       </div>
@@ -51,8 +78,6 @@ class RvrPage extends Component {
 }
 
 export default RvrPage;
-
-
 
 // Original
 // import React, { Component } from "react";
