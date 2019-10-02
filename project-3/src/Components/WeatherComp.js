d1c98b5c90c07e3165bed9344ad61efb09834706
Tymothy J Hanks <tymjhanks@gmail.com>
2019-10-02 09:21:50 -0400

    graph works and just need more specific data points

--- project-3/src/Components/WeatherComp.js
+++ project-3/src/Components/WeatherComp.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 // import * as V from 'victory'
 import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme } from "victory";
-
+//Using this as a test to make sure this data works 
 const GraphData = {
   "photos": [
     { quarter: 1, earnings: 13000 },
@@ -15,10 +15,10 @@ const GraphData = {
 const weatherData = [
     {
       "_id": "5d93b1f01bbc6a00045d882a",
-      "Sol": 294,
+      "Sol": 81,
       "First_UTC": "2019-09-24T07:09:32Z",
       "Last_UTC": "2019-09-25T07:49:06Z",
-      "Season": "spring",
+      "Season": "Spring",
       "airTemp": [
         
       ],
@@ -29,10 +29,10 @@ const weatherData = [
     },
     {
       "_id": "5d93b1f01bbc6a00045d882b",
-      "Sol": 95,
+      "Sol": 195,
       "First_UTC": "2019-09-25T07:49:07Z",
       "Last_UTC": "2019-09-26T08:28:42Z",
-      "Season": "winter",
+      "Season": "Winter",
       "airTemp": [
         
       ],
@@ -43,10 +43,10 @@ const weatherData = [
     },
     {
       "_id": "5d93b1f01bbc6a00045d8829",
-      "Sol": 293,
+      "Sol": 112,
       "First_UTC": "2019-09-23T06:29:58Z",
       "Last_UTC": "2019-09-24T07:09:31Z",
-      "Season": "summer",
+      "Season": "Summer",
       "airTemp": [
         
       ],
@@ -57,10 +57,10 @@ const weatherData = [
     },
     {
       "_id": "5d93b1f01bbc6a00045d8828",
-      "Sol": 292,
+      "Sol": 125,
       "First_UTC": "2019-09-22T05:50:23Z",
       "Last_UTC": "2019-09-23T06:29:57Z",
-      "Season": "fall",
+      "Season": "Fall",
       "airTemp": [
         
       ],
@@ -83,7 +83,7 @@ class Weather extends Component {
           <VictoryAxis></VictoryAxis>
           <VictoryAxis
             dependentAxis={true}
-            tickFormat={x => `${x / 10}°F`}
+            tickFormat={x => `${x / -1}°F`}
           ></VictoryAxis>
           <VictoryBar
             data={weatherData}
@@ -98,7 +98,9 @@ class Weather extends Component {
   }
 }
 export default Weather;
-
+//NOTES
+// Use Victory Stack to stack more data points on the same graph without making another graph and you can use different data points meaning if you wanted to add venus and other planets this would be the best way!
+//Click this link to be able to see what we can do with the data points - https://formidable.com/open-source/victory/docs/
 //Earth Season for x axis and the tempeture on mars for y axis
 //Earth Season for x axis and the tempeture on venus for y axis
 //Earth Season for x axis and the tempeture on saturn for y axis
