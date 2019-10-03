9cf152a55fca4fd3eb02bffebc89add12fbf4b0d
tiffanymesser <tiffanymesser15@gmail.com>
2019-10-03 13:52:14 -0400

    Nothing left to do but debug and integrate with Zachs app

--- project-3/src/App.js
+++ project-3/src/App.js
@@ -2,13 +2,16 @@ import React, { Component } from "react";
 import RvrImg from "./components/RvrImg";
 import RvrPage from "./components/RvrPage";
 import "./App.css";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import VideoBackground from "./components/VideoBackground"
 import Audio from "./components/Audio"
 
 //Tiff adding Redux stuff
 import { Provider } from "react-redux";
 import store from "./store";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
 
 //Tym's Component Imports
 import NavComp from "./components/NavComp";
@@ -20,6 +23,30 @@ import * as V from 'victory'
 //Tiff's Component Improts 
 import Register from "./auth/Register"
 import Login from "./auth/Login"
+import PrivateRoute from "./components/private-route/PrivateRoute";
+import Dashboard from "./components/dashboard/Dashboard";
+
+
+// Tiff's check for token to keep user logged in
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(token);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+
+// Check for expired token
+const currentTime = Date.now() / 1000; // to get in milliseconds
+if (decoded.exp < currentTime) {
+  // Logout user
+  store.dispatch(logoutUser());
+  // Redirect to login
+  window.location.href = "./login";
+}
+}
+
 
 class App extends Component {
   constructor(props) {
@@ -81,9 +108,15 @@ class App extends Component {
       <div className="App">
 
         <Provider store={store}>
+        <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+            <Switch>
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
+            </Switch>
           <Login />
           <Register />
         </Provider>
+       
 
         {/* <Audio />
         <VideoBackground />
