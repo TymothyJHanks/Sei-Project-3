47e460927aff90ee8f5b620dc9b5e98d06169e96
Tymothy J Hanks <tymjhanks@gmail.com>
2019-10-04 09:56:41 -0400

    cleaning up

--- project-3/src/components/CreatePageComp.js
+++ project-3/src/components/CreatePageComp.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "../styles/CreatePageComp.css";
+import "../styles/CreatePage.css";
 class CreatePageComp extends Component {
   constructor(props) {
     super(props);
--- project-3/src/components/WeatherCompBackUp.js
+++ project-3/src/components/WeatherCompBackUp.js
@@ -11,151 +11,11 @@ import {
 } from "victory";
 //Importing CSS
 import "../styles/WeatherBack.css";
-//Using this as a test to make sure this data works
-const GraphData = {
-  photos: [
-    { quarter: 1, earnings: 13000 },
-    { quarter: 2, earnings: 16500 },
-    { quarter: 3, earnings: 14250 },
-    { quarter: 4, earnings: 19000 }
-  ]
-};
+
+
 //VERY IMPORTANT - the 'Sol' data point needs to be equal to the tempature on mars and the "Season" data point is equal to the season on Earth"
 //So find out in spring, whats the tempature on mars and plug that tempature into the 'Sol' data point
 const weatherData = [
-  {
-    _id: "5d94b8efd07b4c00043dbae7",
-    Sol: 295,
-    av: -69.706,
-    ct: 177459,
-    mn: -102.089,
-    mx: -26.606,
-    First_UTC: "2019-09-25T07:49:07Z",
-    Last_UTC: "2019-09-26T08:28:42Z",
-    Season: "spring",
-    pre: [],
-    __v: 0
-  },
-  {
-    _id: "5d94b8efd07b4c00043dbae5",
-    Sol: 293,
-    av: -61.575,
-    ct: 297434,
-    mn: -102.044,
-    mx: -26.367,
-    First_UTC: "2019-09-23T06:29:58Z",
-    Last_UTC: "2019-09-24T07:09:31Z",
-    Season: "spring",
-    pre: [],
-    __v: 0
-  },
-  {
-    _id: "5d94b8efd07b4c00043dbae4",
-    Sol: 292,
-    av: -68.784,
-    ct: 174137,
-    mn: -101.153,
-    mx: -26.434,
-    First_UTC: "2019-09-22T05:50:23Z",
-    Last_UTC: "2019-09-23T06:29:57Z",
-    Season: "spring",
-    pre: [],
-    __v: 0
-  },
-  {
-    _id: "5d94b8efd07b4c00043dbaea",
-    Sol: 298,
-    av: -75.574,
-    ct: 224624,
-    mn: -104.139,
-    mx: -26.833,
-    First_UTC: "2019-09-28T09:47:54Z",
-    Last_UTC: "2019-09-29T10:27:28Z",
-    Season: "spring",
-    pre: [],
-    __v: 0
-  },
-  {
-    _id: "5d94b8efd07b4c00043dbaec",
-    Sol: 300,
-    av: -67.873,
-    ct: 274576,
-    mn: -103.151,
-    mx: -25.6,
-    First_UTC: "2019-09-30T11:07:04Z",
-    Last_UTC: "2019-10-01T11:46:38Z",
-    Season: "spring",
-    pre: [],
-    __v: 0
-  },
-  {
-    _id: "5d94b8efd07b4c00043dbae6",
-    Sol: 294,
-    av: -64.969,
-    ct: 295273,
-    mn: -100.407,
-    mx: -24.846,
-    First_UTC: "2019-09-24T07:09:32Z",
-    Last_UTC: "2019-09-25T07:49:06Z",
-    Season: "spring",
-    pre: [],
-    __v: 0
-  },
-  {
-    _id: "5d94b8efd07b4c00043dbae9",
-    Sol: 297,
-    av: -71.11,
-    ct: 201018,
-    mn: -103.733,
-    mx: -26.088,
-    First_UTC: "2019-09-27T09:08:18Z",
-    Last_UTC: "2019-09-28T09:47:53Z",
-    Season: "spring",
-    pre: [],
-    __v: 0
-  },
-  {
-    _id: "5d94b8efd07b4c00043dbae8",
-    Sol: 296,
-    av: -63.258,
-    ct: 230447,
-    mn: -102.893,
-    mx: -26.39,
-    First_UTC: "2019-09-26T08:28:43Z",
-    Last_UTC: "2019-09-27T09:08:17Z",
-    Season: "spring",
-    pre: [],
-    __v: 0
-  },
-  {
-    _id: "5d94b8efd07b4c00043dbaeb",
-    Sol: 299,
-    av: -69.939,
-    ct: 180108,
-    mn: -101.252,
-    mx: -27.67,
-    First_UTC: "2019-09-29T10:27:29Z",
-    Last_UTC: "2019-09-30T11:07:03Z",
-    Season: "spring",
-    pre: [],
-    __v: 0
-  },
-  {
-    _id: "5d94b8efd07b4c00043dbaed",
-    Sol: 301,
-    av: -71.768,
-    ct: 208772,
-    mn: -103.886,
-    mx: -26.371,
-    First_UTC: "2019-10-01T11:46:39Z",
-    Last_UTC: "2019-10-02T06:00:51Z",
-    Season: "spring",
-    pre: [],
-    __v: 0
-  }
-];
-const weatherData2 = [
-  //This is not being used and  an delete
   {
     _id: "5d94b8efd07b4c00043dbae7",
     Sol: 295,
@@ -295,7 +155,7 @@ class Weather extends Component {
     return (
       <div className="WeatherWrapper">
         <div className="chartWrapper">
-          <h3 className="xAxis">Soloar Date</h3>
+          <h3 className="xAxis">Solar Date</h3>
           <h3 className="yAxis">Tempature</h3>
           <VictoryChart domainPadding={3} theme={VictoryTheme.material}>
             {/* This is the chart ledgend */}
